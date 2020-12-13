95fb5e7571c7d41e20cea136a521011c82c43f0a
Eugene <prokhorov.eu@icloud.com>
2020-12-13 19:38:01 +0400

    4

--- LoginAndPassword/VC/ViewController.swift
+++ LoginAndPassword/VC/ViewController.swift
@@ -25,22 +25,22 @@ class ViewController: UIViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard segue.identifier == "logIn" else { return }
-        let tabBarController = segue.destination as! UITabBarController
-        let destinationVC = tabBarController.navigationController?.viewControllers.first as! ProfileViewController
-        if SGUUser.login.rawValue == loginField.text! && SGUUser.password.rawValue == passwordField.text! {
-            destinationVC.login = loginField.text
-            destinationVC.password = passwordField.text
-        } else {
-            let alertVC = UIAlertController(
-                        title: "Error",
-                        message: "Неверный логин или пароль",
-                        preferredStyle: .alert)
-            let action = UIAlertAction(title: "OK", style: .default, handler: nil)
-            alertVC.addAction(action)
-            self.present(alertVC, animated: true, completion: nil)
-            print("АЛЛО \(SGUUser.login.rawValue)")
-        }
+        //  guard segue.identifier == "logIn" else { return }
+          if SGUUser.login.rawValue == loginField.text && SGUUser.password.rawValue == passwordField.text {
+              let tabBarController = segue.destination as! UITabBarController
+              let destinationVC = tabBarController.navigationController?.viewControllers.first as! ProfileViewController
+              destinationVC.login = loginField.text
+              destinationVC.password = passwordField.text
+          } else {
+              let alertVC = UIAlertController(
+                          title: "Error",
+                          message: "Неверный логин или пароль",
+                          preferredStyle: .alert)
+              let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+              alertVC.addAction(action)
+              self.present(alertVC, animated: true, completion: nil)
+              print("АЛЛО \(SGUUser.login.rawValue)")
+          }
     }
     
     @IBAction func unwind (_ seg: UIStoryboardSegue) {
